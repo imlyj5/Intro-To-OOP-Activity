f57791c26ed93e4c352dc5e73427209d45957ef6
Jeslyn Lai <imlyj5@gmail.com>
2025-03-27 11:41:10 -0700

    implements student class and get_student_with_more_classes function

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,8 @@
 # add your get_student_with_more_classes function here!
+def get_student_with_more_classes(student_1, student_2):
+    if student_1.get_num_classes() > student_2.get_num_classes():
+        print(f"{student_1.name } has more class")
+        return student_1 
+    else:
+        print(f"{student_2.name } has more class")
+        return student_2
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,22 @@
 # add your Student class here!
+class Student:
+    def __init__(self,name,grade,classes):
+        self.name = name
+        self.grade = grade
+        self.classes = classes
+
+    def add_class(self, class_to_add):
+        self.classes.append(class_to_add)
+        return self.classes
+
+
+    def get_num_classes(self):
+        return len(self.classes)
+    
+
+    def summary(self):
+        #"Samara is a junior enrolled in 7 classes"
+        print(f"{self.name} is a {self.grade} has {self.get_num_classes()} classes.")
+
+
+    
